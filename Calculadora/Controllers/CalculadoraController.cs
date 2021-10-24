b37037b6c8eee8d671a22c7f449020b30f69d2ce
Luiz Otavio <luiz_otavio71@hotmail.com>
2021-10-24 15:26:35 -0300

    criando uma calculadora básica

---
+++ Calculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,45 @@
+﻿using Calculadora.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Calcular()
+        {
+            return View(new Calc());
+        }
+
+        [HttpPost]
+        public   IActionResult Calcular(Calc calc , string c)
+        {
+            var div = 0;
+            if (c == "calc")
+            {
+                calc.tot = calc.no1 + calc.no2;
+            } else if(c=="dim")
+            {
+                calc.tot = calc.no1 - calc.no2;
+            }
+            else if(c=="divv")
+            {
+                
+                div = calc.no1 / calc.no2;
+
+            }
+            else if(c == "mult")
+            {
+                calc.tot = (calc.no1 * calc.no2);
+                
+            } else
+            {
+                return View(calc);
+            }
+            return   View(calc);
+        }
+    }
+}
---
+++ Calculadora/Models/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Models
+{
+    public class Calc
+    {
+        public Calc(int no1, int no2, int tot)
+        {
+            this.no1 = no1;
+            this.no2 = no2;
+            this.tot = tot;
+        }
+
+        public Calc()
+        {
+        }
+
+        [Display(Name ="Nº 1 ")]
+        public int no1 { get; set; }
+        [Display(Name = "Nº 2 ")]
+        public int no2 { get; set; }
+        public int tot { get; set; }
+    }
+}
